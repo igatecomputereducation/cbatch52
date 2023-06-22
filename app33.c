eef6f86226619adc5e8bfe35d14bd2c8459df017
igate <igatecomputereducation@gmail.com>
2023-06-22 10:57:57 +0530

    v1

---
+++ app33.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int qty;
+	float price,bill,dis,nbill;
+	printf("Price:");
+	scanf("%f",&price);
+	printf("Quantity:");
+	scanf("%i",&qty);
+	bill=price*qty;
+	
+	if(bill>10000)
+		dis=bill*5/100;
+	if(bill<=10000)	
+		dis=bill*3/100;
+	
+	nbill=bill-dis;
+	printf("\n-----------------------\n");
+	printf("Bill %.2f\n",bill);
+	printf("Discount %.2f\n",dis);
+	printf("Netbill %.2f",nbill);
+	return 0;
+}
---
+++ app34.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int ch;
+	printf("1.Permanent\n2.Temporary\nEnter your choice [1..2]:");
+	scanf("%i",&ch);
+	
+	if(ch==1)
+	{
+		printf("Narayana murthi\n");
+		printf("Jaladanki");
+	}
+	if(ch==2)
+	{
+		printf("Narayana murthi\n");
+		printf("c/o igate solutions");
+	}
+	if(ch<1)
+		printf("Invalid choice");
+	if(ch>2)
+		printf("Invalid choice");
+	
+	return 0;
+}
