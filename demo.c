1e6e0fb151d7ccb3410d8312f85e72cd365a1c6d
igatecomputereducation <igatecomputereducation@gmail.com>
2023-08-21 11:13:40 +0530

    v1

--- demo.c
+++ demo.c
@@ -1,12 +1,28 @@
+/*
+Descr: To find biggest number
+Author: subbu
+Date: 07/08/2023
+Version 1.0
+Loc: TCS, Bangalore
+*/
 #include<stdio.h>
 int main()
 {
-	int i;
-	for(i=1;i<=10;i++)
-	{
-		if(i%2==0)
-			continue;
-		printf("%i\n",i);
-	}
+	/*Declaration of variables*/
+	int x,y;
+	
+	//Taking input
+	printf("Enter 2 integers\n");
+	scanf("%i%i",&x,&y);
+	
+	//logic to print biggest number
+	if(x==y)
+		printf("Equals");
+	else if(x>y)
+		printf("Biggest number %i",x);
+	else
+		printf("Biggest number %i",y);
+		
 	return 0;
 }
+
